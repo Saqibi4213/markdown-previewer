6b570337c10aadcabe0b861c9aa17bf4cadf4ae9
Saqibi4213 <mehriamosawer@gmail.com>
2024-08-20 00:20:41 +0430

    index.js linter error handling

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
+// eslint-disable-next-line import/extensions
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-// eslint-disable-next-line import/extensions
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
