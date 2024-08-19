26c82dd3932611b33d7a1866257748a244413bbd
Saqibi4213 <mehriamosawer@gmail.com>
2024-08-20 00:25:17 +0430

    app is not extenetion with js solved in index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 // eslint-disable-next-line no-unused-vars
-import App from './App';
+import App from './App.js';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
