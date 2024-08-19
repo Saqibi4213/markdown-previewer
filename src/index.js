49f9e2369b1ad4e90a0c513739ef57c1bc2cb620
Saqibi4213 <mehriamosawer@gmail.com>
2024-08-20 00:26:56 +0430

    More than 1 blank line not allowed  no-multiple-empty-lines solveddd

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import './index.css';
 // eslint-disable-next-line no-unused-vars
 import App from './App.js';
 
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
