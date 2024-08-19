cfd0b24191990f7c188c46fe639d53412b6e80f6
Saqibi4213 <mehriamosawer@gmail.com>
2024-08-20 00:06:29 +0430

    add js extension in import in app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 // eslint-disable-next-line no-unused-vars
-import MarkdownPreviwer from'./components/MarkdownPreviwer';
+import MarkdownPreviwer from './components/MarkdownPreviwer.js';
 
 function App() {
   return (
